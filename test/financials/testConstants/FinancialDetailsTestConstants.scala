73e9d03379acfb0d9c49e53058ab6f89b510a0f9
kSiadat <49532979+kSiadat@users.noreply.github.com>
2026-09-04 13:00:13 +0100

    MIPR-2991: ran migrate.sh

--- test/financials/testConstants/FinancialDetailsTestConstants.scala
+++ test/financials/testConstants/FinancialDetailsTestConstants.scala
@@ -900,7 +900,7 @@ object FinancialDetailsTestConstants {
     )
   )
 
-  lazy val testValidFinancialDetailsModelWithBalancingCharge: FinancialDetailsModel = FinancialDetailsModel(
+  def testValidFinancialDetailsModelWithBalancingCharge(dunningLock: Option[String] = None): FinancialDetailsModel = FinancialDetailsModel(
     balanceDetails = BalanceDetails(1.00, 2.00, 0.00, 3.00, None, None, None, None, None, None, None),
     documentDetails = List(
       DocumentDetail(taxYear = 2019,
@@ -940,7 +940,7 @@ object FinancialDetailsTestConstants {
           dueDate = Some(LocalDate.parse("2019-05-15")),
           subItemId = Some("1"),
           amount = Some(100),
-          dunningLock = Some("Stand over order"),
+          dunningLock = dunningLock,
           interestLock = Some("interestLock"),
           clearingDate = Some(LocalDate.parse("2020-08-16")),
           clearingReason = Some("clearingReason"),
@@ -995,7 +995,7 @@ object FinancialDetailsTestConstants {
           dueDate = Some(LocalDate.parse("2019-05-15")),
           subItemId = Some("1"),
           amount = Some(100),
-          dunningLock = Some("Stand over order"),
+//          dunningLock = Some("Stand over order"),
           interestLock = Some("interestLock"),
           clearingDate = Some(LocalDate.parse("2020-08-16")),
           clearingReason = Some("clearingReason"),
@@ -1010,7 +1010,7 @@ object FinancialDetailsTestConstants {
     )
   )
 
-  lazy val testValidFinancialDetailsModelWithLateSubmissionPenalty: FinancialDetailsModel = FinancialDetailsModel(
+  def testValidFinancialDetailsModelWithLateSubmissionPenalty(dunningLock: Option[String] = None): FinancialDetailsModel = FinancialDetailsModel(
     balanceDetails = BalanceDetails(1.00, 2.00, 0.00, 3.00, None, None, None, None, None, None, None),
     documentDetails = List(
       DocumentDetail(taxYear = 2019,
@@ -1050,7 +1050,7 @@ object FinancialDetailsTestConstants {
           dueDate = Some(LocalDate.parse("2019-05-15")),
           subItemId = Some("1"),
           amount = Some(100),
-          dunningLock = Some("Stand over order"),
+          dunningLock = dunningLock,
           interestLock = Some("interestLock"),
           clearingDate = Some(LocalDate.parse("2020-08-16")),
           clearingReason = Some("clearingReason"),
@@ -1066,7 +1066,7 @@ object FinancialDetailsTestConstants {
   )
 
 
-  lazy val testValidFinancialDetailsModelWithLspCrystallisedInterest: FinancialDetailsModel = FinancialDetailsModel(
+  def testValidFinancialDetailsModelWithLspCrystallisedInterest(dunningLock: Option[String] = None): FinancialDetailsModel = FinancialDetailsModel(
     balanceDetails = BalanceDetails(1.00, 2.00, 0.00, 3.00, None, None, None, None, None, None, None),
     documentDetails = List(
       DocumentDetail(taxYear = 2019,
@@ -1106,7 +1106,7 @@ object FinancialDetailsTestConstants {
           dueDate = Some(LocalDate.parse("2019-05-15")),
           subItemId = Some("1"),
           amount = Some(100),
-          dunningLock = Some("Stand over order"),
+          dunningLock = dunningLock,
           interestLock = Some("interestLock"),
           clearingDate = Some(LocalDate.parse("2020-08-16")),
           clearingReason = Some("clearingReason"),
@@ -1121,7 +1121,7 @@ object FinancialDetailsTestConstants {
     )
   )
 
-  lazy val testValidFinancialDetailsModelWithLatePaymentPenalty: FinancialDetailsModel = FinancialDetailsModel(
+  def testValidFinancialDetailsModelWithLatePaymentPenalty(dunningLock: Option[String] = None): FinancialDetailsModel = FinancialDetailsModel(
     balanceDetails = BalanceDetails(1.00, 2.00, 0.00, 3.00, None, None, None, None, None, None, None),
     documentDetails = List(
       DocumentDetail(taxYear = 2021,
@@ -1161,7 +1161,7 @@ object FinancialDetailsTestConstants {
           dueDate = Some(LocalDate.parse("2019-05-15")),
           subItemId = Some("1"),
           amount = Some(100),
-          dunningLock = Some("Stand over order"),
+          dunningLock = dunningLock,
           interestLock = Some("interestLock"),
           clearingDate = Some(LocalDate.parse("2020-08-16")),
           clearingReason = Some("clearingReason"),
@@ -1176,7 +1176,7 @@ object FinancialDetailsTestConstants {
     )
   )
 
-  lazy val testValidFinancialDetailsModelWithBalancingChargeWithAccruingInterest: FinancialDetailsModel = FinancialDetailsModel(
+  def testValidFinancialDetailsModelWithBalancingChargeWithAccruingInterest(dunningLock: Option[String] = None): FinancialDetailsModel = FinancialDetailsModel(
     balanceDetails = BalanceDetails(1.00, 2.00, 0.00, 3.00, None, None, None, None, None, None, None),
     documentDetails = List(
       DocumentDetail(taxYear = 2019,
@@ -1216,7 +1216,7 @@ object FinancialDetailsTestConstants {
           dueDate = Some(LocalDate.parse("2019-05-15")),
           subItemId = Some("1"),
           amount = Some(100),
-          dunningLock = Some("Stand over order"),
+          dunningLock = dunningLock,
           interestLock = Some("interestLock"),
           clearingDate = Some(LocalDate.parse("2020-08-16")),
           clearingReason = Some("clearingReason"),
@@ -1271,7 +1271,7 @@ object FinancialDetailsTestConstants {
           dueDate = Some(LocalDate.parse("2019-05-15")),
           subItemId = Some("1"),
           amount = Some(100),
-          dunningLock = Some("Stand over order"),
+//          dunningLock = Some("Stand over order"),
           interestLock = Some("interestLock"),
           clearingDate = Some(LocalDate.parse("2020-08-16")),
           clearingReason = Some("clearingReason"),
@@ -2494,12 +2494,12 @@ case class ANewCreditAndRefundModel(model: CreditsModel = CreditsModel(0.0, 0.0,
 
   def withFirstRefund(amount: BigDecimal) = {
     ANewCreditAndRefundModel(model.copy(transactions = model.transactions :+
-      Transaction(Repayment, amount, taxYear = None, None, None, "refund1")))
+      Transaction(Repayment, amount, taxYear = None, None, None, None, "refund1")))
   }
 
   def withSecondRefund(amount: BigDecimal) = {
     ANewCreditAndRefundModel(model.copy(transactions = model.transactions :+
-      Transaction(Repayment, amount, taxYear = None, None, None, "refund2")))
+      Transaction(Repayment, amount, taxYear = None, None, None, None, "refund2")))
   }
   
   def withBalancingChargeCredit(dueDate: LocalDate, outstandingAmount: BigDecimal, id: String = "balancing") = {
@@ -2542,40 +2542,40 @@ case class ANewCreditAndRefundModel(model: CreditsModel = CreditsModel(0.0, 0.0,
 object TestTransactions: 
 
   def itsaReturnAmendmentCredit(dueDate: LocalDate, outstandingAmount: BigDecimal, id: String = "IRA-credit"): Transaction =
-    Transaction(ITSAReturnAmendmentCredit, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = Some(dueDate), None, id)
+    Transaction(ITSAReturnAmendmentCredit, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = None, documentDate = Some(dueDate), None, id)
 
-  def poaOneReconciliationCredit(dueDate: LocalDate, outstandingAmount: BigDecimal, id: String = "POA1RR-credit"): Transaction = 
-    Transaction(PoaOneReconciliationCredit, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = Some(dueDate), None, id)
+  def poaOneReconciliationCredit(dueDate: LocalDate, outstandingAmount: BigDecimal, id: String = "POA1RR-credit"): Transaction =
+    Transaction(PoaOneReconciliationCredit, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = None, documentDate = Some(dueDate), None, id)
 
-  def poaTwoReconciliationCredit(dueDate: LocalDate, outstandingAmount: BigDecimal, id: String = "POA2RR-credit"): Transaction = 
-    Transaction(PoaTwoReconciliationCredit, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = Some(dueDate), None, id)
+  def poaTwoReconciliationCredit(dueDate: LocalDate, outstandingAmount: BigDecimal, id: String = "POA2RR-credit"): Transaction =
+    Transaction(PoaTwoReconciliationCredit, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = None, documentDate = Some(dueDate), None, id)
 
-  def balancingChargeCredit(dueDate: LocalDate, outstandingAmount: BigDecimal, id: String = "balancing"): Transaction = 
-    Transaction(BalancingChargeCreditType, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = Some(dueDate), None, id)
+  def balancingChargeCredit(dueDate: LocalDate, outstandingAmount: BigDecimal, id: String = "balancing"): Transaction =
+    Transaction(BalancingChargeCreditType, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = None, documentDate = Some(dueDate), None, id)
 
-  def cutOverCredit(dueDate: LocalDate, outstandingAmount: BigDecimal): Transaction = 
-    Transaction(CutOverCreditType, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = Some(dueDate), None, "cutover")
+  def cutOverCredit(dueDate: LocalDate, outstandingAmount: BigDecimal): Transaction =
+    Transaction(CutOverCreditType, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = None, documentDate = Some(dueDate), None, "cutover")
 
-  def hmrcAutoCorrection(dueDate: LocalDate, outstandingAmount: BigDecimal): Transaction = 
-    Transaction(ITSAReturnAmendmentCredit, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = Some(dueDate), None, "IRA-credit", Some("AC"))
+  def hmrcAutoCorrection(dueDate: LocalDate, outstandingAmount: BigDecimal): Transaction =
+    Transaction(ITSAReturnAmendmentCredit, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = None, documentDate = Some(dueDate), None, "IRA-credit", Some("AC"))
 
-  def hmrcManualCorrection(dueDate: LocalDate, outstandingAmount: BigDecimal): Transaction = 
-    Transaction(ITSAReturnAmendmentCredit, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = Some(dueDate), None, "IRA-credit", Some("AC"))
+  def hmrcManualCorrection(dueDate: LocalDate, outstandingAmount: BigDecimal): Transaction =
+    Transaction(ITSAReturnAmendmentCredit, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = None, documentDate = Some(dueDate), None, "IRA-credit", Some("AC"))
 
-  def mfaCredit(dueDate: LocalDate, outstandingAmount: BigDecimal): Transaction = 
-    Transaction(MfaCreditType, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = Some(dueDate), None, "mfa")
+  def mfaCredit(dueDate: LocalDate, outstandingAmount: BigDecimal): Transaction =
+    Transaction(MfaCreditType, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = None, documentDate = Some(dueDate), None, "mfa")
 
-  def repaymentInterestCredit(dueDate: LocalDate, outstandingAmount: BigDecimal): Transaction = 
-    Transaction(RepaymentInterest, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = Some(dueDate), None, "repayment")
+  def repaymentInterestCredit(dueDate: LocalDate, outstandingAmount: BigDecimal): Transaction =
+    Transaction(RepaymentInterest, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = None, documentDate = Some(dueDate), None, "repayment")
 
   def refund(amount: Double, number: Int): Transaction = 
-    Transaction(Repayment, BigDecimal(amount), taxYear = None, None, None, s"refund$number")
+    Transaction(Repayment, BigDecimal(amount), taxYear = None, None, None, None, s"refund$number")
     
   def payment(amount: Double, number: Option[Int]): Transaction = 
-    Transaction(PaymentType, BigDecimal(amount), taxYear = None, None, None, s"payment${number.getOrElse("")}")
+    Transaction(PaymentType, BigDecimal(amount), taxYear = None, None, None, None, s"payment${number.getOrElse("")}")
 
-  def payment(dueDate: LocalDate, outstandingAmount: BigDecimal, effectiveDateOfPayment: Option[LocalDate] = None): Transaction = 
-    Transaction(PaymentType, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = Some(dueDate), Some(effectiveDateOfPayment.getOrElse(dueDate)), "payment")
+  def payment(dueDate: LocalDate, outstandingAmount: BigDecimal, effectiveDateOfPayment: Option[LocalDate] = None): Transaction =
+    Transaction(PaymentType, outstandingAmount, taxYear = Some(TaxYear.getTaxYear(dueDate)), dueDate = Some(dueDate), None, Some(effectiveDateOfPayment.getOrElse(dueDate)), "payment")
 
 object CreditAndRefundConstants {
   def balanceDetailsModel(firstPendingAmountRequested: Option[BigDecimal] = Some(3.50),
