59c4ae10671a89df78a9ebd9d7df75aba8712208
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2026-09-11 11:16:09 +0100

    MIPR-3060-a removing temp code added to test NRS in Prod

--- migrate.sh
+++ migrate.sh
@@ -9,9 +9,6 @@ cp -r app/common/config/FrontendAppConfig.scala tmp/.
 cp app/common/models/incomeSourceDetails/IncomeSourceDetailsResponse.scala tmp/.
 cp app/common/models/liabilitycalculation/LiabilityCalculationResponse.scala tmp/.
 cp app/shared/enums/JourneyState.scala tmp/.
-cp app/common/config/Initialise.scala tmp/.
-cp app/financials/connectors/NrsConnectorTmp.scala tmp/.
-cp app/common/config/DIModule.scala tmp/.
 
 # ========== feature switch ==============
 cp app/common/models/admin/FeatureSwitchName.scala tmp/.
@@ -48,10 +45,6 @@ cp tmp/FeatureSwitchConnector.scala app/common/connectors/.
 cp tmp/PoaAmendmentData.scala app/financials/models/claimToAdjustPoa/.
 cp tmp/AuditingService.scala app/common/services/.
 cp tmp/InternalUrlHelper.scala app/common/viewUtils/.
-
-cp tmp/Initialise.scala app/common/config/.
-cp tmp/NrsConnectorTmp.scala app/financials/connectors/NrsConnector.scala
-cp tmp/DIModule.scala app/common/config/.
 
 
 echo "Removing unused files"
